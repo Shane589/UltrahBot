e6382c78c584a59460c83698af3f3b7ac53a2847
Shane <147728505+Shane589@users.noreply.github.com>
2026-03-20 13:12:30 +0000

    litteraly half of the ban command lol

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,9 +1,45 @@
 const {
   ApplicationCommandOptionType,
   PermissionFlagsBits,
+  Client,
+  Interaction
 } = require("discord.js");
 
 module.exports = {
+  /**
+   * 
+   * @param {Client} client 
+   * @param {Interaction} interaction 
+   */
+
+  callback: async (client, interaction) {
+    const targetUserId = interaction.options.get('target-user').value();
+    const reason = interaction.options.get('reason')?.value() || "No reason provided.";
+
+    await interaction.deferReply();
+
+    const targetUser = await interaction.guild.members.fetch(targetUserId);
+
+    if (!targetUser) {
+      await interaction.editReply("That user doesn't exist in this guild?");
+      return;
+    }
+
+    if (targetUser.id === interaction.guild.ownerId) {
+      await interaction.editReply("This user is the owner of this guild.");
+      return;
+    }
+
+    const targetUserRolePosition = targetUser.roles.highest.position;
+    const requestUserRolePosition = interaction.member.roles.highest.position;
+    const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+    if (targetUserRolePosition >= requestUserRolePosition) {
+      await interaction.editReply("You can not ban a higher rank than yourself ):");
+      return;
+    }
+  },
+
   name: "ban",
   description: "Bans a member from the server.",
   // devOnly: Boolean,
@@ -23,8 +59,4 @@ module.exports = {
   ],
   permissionsRequired: [PermissionFlagsBits.BanMembers],
   botPermissions: [PermissionFlagsBits.Administrator],
-
-  callback: (client, interaction) => {
-    interaction.reply("Ban...");
-  },
 };
